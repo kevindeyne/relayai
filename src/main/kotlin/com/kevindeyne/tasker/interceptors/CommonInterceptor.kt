b9c3e969aa9bcee79ddfbf900c724f739342e4e7
Kevin Deyne <kevindeyne@gmail.com>
2018-01-27 16:13:04 -0800

    Interceptor filtering

--- src/main/kotlin/com/kevindeyne/tasker/interceptors/CommonInterceptor.kt
+++ src/main/kotlin/com/kevindeyne/tasker/interceptors/CommonInterceptor.kt
@@ -12,28 +12,40 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class CommonInterceptor(val issueRepository : IssueRepository) : HandlerInterceptorAdapter() {
+	
+	val blacklist = listOf("pull", "login", "logout", "error",
+			"/stylesheets/", "/js/", "/icons/", "/fonts/")
+	
+	override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any?) : Boolean {		
+		if(isValidHandle(request)){
+			val reqUrl = request.getRequestURL().toString()
+			if(SecurityHolder.getProjectId() == -1L && !reqUrl.contains("/project/new")){
+				response.sendRedirect("/project/new");							
+				return false
+			}
+		}
+
+		return true
+	}
 
 	override fun postHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any?, model: ModelAndView?) {
-		if(model != null){
-			val context = SecurityContextHolder.getContext()
-			if(context != null && context.authentication != null){
-				val authentication : Authentication = context.getAuthentication()
-				if (authentication != null && !(authentication is AnonymousAuthenticationToken)) {
-					val reqUrl = request.getRequestURL().toString()
-					if(reqUrl.contains("pull") || reqUrl.contains("login") || reqUrl.contains("logout")) {
-						return;
-					}
-				
-					if (SecurityHolder.hasRole(Role.DEVELOPER) || SecurityHolder.hasRole(Role.TESTER)) {					
-						if(SecurityHolder.getProjectId() == -1L && !reqUrl.contains("/project/new")){
-							response.sendRedirect("/project/new");
-							return
-						}
-						
-						model.addObject("inProgressIssueList", issueRepository.findAllInProgress())
-					}  
-				}	
-			}
+		if(model != null && isValidHandle(request)){		
+			if (SecurityHolder.hasRole(Role.DEVELOPER) || SecurityHolder.hasRole(Role.TESTER)) {					
+				model.addObject("inProgressIssueList", issueRepository.findAllInProgress())
+			} 
 		}				
+	}
+	
+	fun isValidHandle(request: HttpServletRequest) : Boolean {
+		val context = SecurityContextHolder.getContext()
+		if(context != null && context.authentication != null){
+			val authentication : Authentication = context.getAuthentication()
+			if (!(authentication is AnonymousAuthenticationToken)) {				
+				val reqUrl = request.getRequestURL().toString()
+				for(listitem in blacklist){ if(reqUrl.contains(listitem)) { return false; } }				
+				return true
+			}
+		}
+		return false
 	}
 }
